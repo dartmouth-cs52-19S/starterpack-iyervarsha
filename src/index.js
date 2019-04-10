86c1189542d4fd796f9e21713a37b8a038f6b109
Varsha Iyer <varsha.b.iyer@gmail.com>
2019-04-10 18:37:39 -0400

    surge website

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("starting up!");
+import $ from 'jquery';
+import './style.scss';
+
+let num = 0;
+const printFunction = () => {
+  num += 1;
+  $('#main').html(`You've been on this page for ${num} seconds.`);
+};
+
+setInterval(printFunction, 1000);
