4a479af666ec601a31053f11b4a912821a278c99
Varsha Iyer <varsha.b.iyer@gmail.com>
2019-04-26 00:28:39 -0400

    SA6 done

---
+++ src/components/app.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import '../style.scss';
+import {
+  BrowserRouter as Router, Route, Switch, NavLink,
+} from 'react-router-dom';
+import Counter from '../containers/counter';
+import Controls from '../containers/controls';
+
+// keys for actiontypes
+export const ActionTypes = {
+  INCREMENT: 'INCREMENT',
+  DECREMENT: 'DECREMENT',
+};
+
+
+export function increment() {
+  return {
+    type: ActionTypes.INCREMENT,
+    payload: null,
+  };
+}
+
+export function decrement() {
+  return {
+    type: ActionTypes.DECREMENT,
+    payload: null,
+  };
+}
+
+const About = (props) => {
+  return <div> All there is to know about me </div>;
+};
+
+const Welcome = (props) => {
+  return <div>Welcome<Counter /><Controls /></div>;
+};
+
+const Nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/" exact>Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
+
+const Test = (props) => {
+  return <div> ID: {props.match.params.id} </div>;
+};
+
+const FallBack = (props) => {
+  return <div>URL Not Found</div>;
+};
+
+const App = (props) => {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/about" component={About} />
+          <Route exact path="/test/:id" component={Test} />
+          <Route component={FallBack} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
